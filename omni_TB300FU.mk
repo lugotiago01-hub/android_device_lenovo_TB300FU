9096b9fdfd3e663ff12c1863c248d08f2f28aa20
lugotiago01-hub <lugotiago01@gmail.com>
2026-08-13 20:31:19 -0300

    Add TWRP Device Tree For Lenovo Tab M8 (TB300FU)

---
+++ omni_TB300FU.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TB300FU device
+$(call inherit-product, device/lenovo/TB300FU/device.mk)
+
+PRODUCT_DEVICE := TB300FU
+PRODUCT_NAME := omni_TB300FU
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := TB300FU
+PRODUCT_MANUFACTURER := lenovo
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_tc422_wifi-user 12 SP1A.210812.016 S001076_221215_ROW release-keys"
+
+BUILD_FINGERPRINT := Lenovo/TB300FU_S/TB300FU:12/SP1A.210812.016/S001076_221215_ROW:user/release-keys
